d36cb1c874f0736bab45554abde3d25170ae89ed
Berry <Sebastianberry11@gmail.com>
2022-09-02 11:40:49 +0200

    Spansk sprog lavet

---
+++ src/Spansk.java
@@ -0,0 +1,16 @@
+public class Spansk extends Dialog{
+    @Override
+    public String hi() {
+        return "Hej på spansk";
+    }
+
+    @Override
+    public String changeLanguage() {
+        return "Ændre sprog på Spansk";
+    }
+
+    @Override
+    public String selectLanguage() {
+        return "Select language dk/eng/sve";
+    }
+}
