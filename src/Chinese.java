fd443d220fc2d68c79f89e7dbf131499a5db25ac
Martin Lorentzen <silverball1@gmail.com>
2022-09-02 11:41:12 +0200

    kinesisk sprog

---
+++ src/Chinese.java
@@ -0,0 +1,16 @@
+public class Chinese extends Dialog{
+    @Override
+    public String hi() {
+        return "Shi shi";
+    }
+
+    @Override
+    public String changeLanguage() {
+        return "skift sprog på kinesisk";
+    }
+
+    @Override
+    public String selectLanguage() {
+        return "Select language dk/eng/sve";
+    }
+}
