aaa9c5db744e7d50d5b7d287ffaabdaeaa808239
MrSalim86 <cph-ms1068@cphbusiness.dk>
2022-09-02 11:44:45 +0200

    Sprog

---
+++ src/Hawaiiansk.java
@@ -0,0 +1,16 @@
+public class Hawaiiansk extends Dialog{
+    @Override
+    public String hi() {
+        return "Hui";
+    }
+
+    @Override
+    public String changeLanguage() {
+        return "makemake oe e ho ololi i ka olelo";
+    }
+
+    @Override
+    public String selectLanguage() {
+        return "koho olelo dk/eng/sve/hawaii";
+    }
+}
